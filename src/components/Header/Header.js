bdc721081707bf58bc455ab28db74801a12b0bb9
rahidhaque <ontirahid@yahoo.com>
2022-04-03 22:13:41 +0600

    Routing Done with 404

---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <h2>Gamer's Goodies Duniya</h2>
+            <nav >
+                <Link to="/home">Home</Link>
+                <Link to="/reviews">Reviews</Link>
+                <Link to='/dashboard'>Dashboard</Link>
+                <Link to='/blogs'>Blogs</Link>
+                <Link to='/About'>About</Link>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
